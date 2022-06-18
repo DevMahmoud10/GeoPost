b425e602893aafab7bd26d2c04128ad82048b929
Mahmoud <mahmoud.ahmed.ibrahim@outlook.com>
2022-06-18 12:57:20 +0200

    refactor find post code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import errno
-from flask import Flask, render_template, request, flash
+
 import geopy
+from flask import Flask, flash, render_template, request
 
 app = Flask(__name__)
 app.secret_key = "manbearbird_MAMUDU"
@@ -20,9 +21,14 @@ def get_postal_code():
     except:
         return render_template("index.html", error="Enter Valid Geo Coordinates")
     try:
-        geolocator = geopy.Nominatim(user_agent='flask-geocoder')
-        address = geolocator.reverse(coordinates)
-        postcode = address.raw['address']['postcode']
+        postcode = find_postcode(coordinates)
     except:
         return render_template("index.html", error="There's a problem with this geo coordinates")
     return render_template("index.html", postcode=postcode, coordinates=coordinates)
+
+
+def find_postcode(coordinates):
+    geolocator = geopy.Nominatim(user_agent='flask-geocoder')
+    address = geolocator.reverse(coordinates)
+    postcode = address.raw['address']['postcode']
+    return postcode
